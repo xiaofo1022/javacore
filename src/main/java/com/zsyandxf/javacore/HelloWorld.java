dc35a520f01ed911ea371196f0eca9df8dcd7b09
zsypfan <siyuan_new2007@sina.com>
2015-08-22 16:41:09 +0800

    hello world 练习~~

--- src/main/java/com/zsyandxf/javacore/HelloWorld.java
+++ src/main/java/com/zsyandxf/javacore/HelloWorld.java
@@ -1,10 +1,7 @@
 package com.zsyandxf.javacore;
 
 public class HelloWorld {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello, Fucking world!");
 	}
-
 }
